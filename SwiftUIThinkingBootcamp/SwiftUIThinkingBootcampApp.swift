5e45296c64e3c941fef629a78a3799699be3bee2
owennguyen3162 <nguyentuan3162@gmail.com>
2024-04-27 17:30:54 +0700

    Initial Commit

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThinkingBootcampApp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Nguyen Anh Tuan on 27/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
