0697d97232983325609f65f6ce31f4a0d52c0116
owennguyen3162 <nguyentuan3162@gmail.com>
2024-04-27 17:48:15 +0700

    Adding Text in SwiftUI

---
+++ SwiftUIThinkingBootcamp/Src/TextBootCamp.swift
@@ -0,0 +1,31 @@
+//
+//  TextBootCamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Nguyen Anh Tuan on 27/4/24.
+//
+
+import SwiftUI
+
+struct TextBootCamp: View {
+    var body: some View {
+        Text("Hello, World! This is swiful Thinking bootcamp. I am really enjoying this course and learning alot.".uppercased())
+//            .font(.body)
+//            .fontWeight(.bold)
+//            .bold()
+//            .underline()
+//            .italic()
+//            .strikethrough(true,color: .red)
+//            .font(.system(size: 30, weight: .semibold, design: .serif))
+//            .multilineTextAlignment(.leading)
+//            .baselineOffset(2)
+//            .kerning(10)
+//            .foregroundColor(.green)
+//            .frame(width: 100, height: 100, alignment: .leading)
+//            .minimumScaleFactor(0.1)
+    }
+}
+
+#Preview {
+    TextBootCamp()
+}
