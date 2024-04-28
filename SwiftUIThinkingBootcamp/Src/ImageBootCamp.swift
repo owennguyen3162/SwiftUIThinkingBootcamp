a2e62de66a2f5d70facd525859b4188c0b597865
owennguyen3162 <nguyentuan3162@gmail.com>
2024-04-28 16:41:00 +0700

    Adding images to a SwiftUI application

---
+++ SwiftUIThinkingBootcamp/Src/ImageBootCamp.swift
@@ -0,0 +1,31 @@
+//
+//  ImageBootCamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Nguyen Anh Tuan on 28/4/24.
+//
+
+import SwiftUI
+
+struct ImageBootCamp: View {
+    var body: some View {
+        Image("TheRock")
+            .resizable()
+//            .aspectRatio(contentMode: .fill)
+//            .scaledToFill()
+            .frame(width: 300, height: 200)
+//            .border(.red, width: 1)
+//            .clipped()
+//            .cornerRadius(30.0)
+            .clipShape(
+//                Circle()
+//                Rectangle()
+//                RoundedRectangle(cornerRadius: 20)
+                Ellipse()
+            )
+    }
+}
+
+#Preview {
+    ImageBootCamp()
+}
