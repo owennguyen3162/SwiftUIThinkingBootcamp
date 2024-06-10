280c5b0a7b8e86878958b6b7b2f8dd6869f41dc7
owennguyen3162 <nguyentuan3162@gmail.com>
2024-06-10 08:31:10 +0700

    InstagramCloneUI

---
+++ SwiftUIThinkingBootcamp/instagramCloneUI/storyItem.swift
@@ -0,0 +1,19 @@
+//
+//  storyItem.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Nguyen Anh Tuan on 9/6/24.
+//
+
+import Foundation
+
+struct StoryItem {
+    var name: String
+    var id: Int
+    
+    init(name: String, id: Int) {
+        self.name = name
+        self.id = id
+    }
+    
+}
